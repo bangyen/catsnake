99d4c2e592d92325642f608f05341708873d62c7
SeriousWeasle <seriousweasle@gmail.com>
2020-07-01 16:41:32 +0200

    copyover

--- catsnake/imag.py
+++ catsnake/imag.py
@@ -11,24 +11,46 @@ class comp:
             real = self.real + other.real
             imag = self.imag + other.imag
             return comp(real, imag)
-        #add complex with other complex
+        
+        #add complex with imaginary
+        if type(other) == "imag":
+            imag = self.imag + other.val
+            return comp(self.real, imag)
+
+        #add complex with real
         else:
             real = self.real + other
+            return comp(real, self.imag)
+    
+    def __sub__(self, other):
+        #subtract complex
+        if type(other) == "comp":
+            real = self.real - other.real
+            imag = self.imag - other.imag
+            return comp(real, imag)
+        else:
+            real = self.real - other
             return comp(real, self.imag)
 
 
 class imag:
+    #initialize imaginary number
     def __init__(self, value):
         self.val = value
     
+    #add
     def __add__(self, other):
+        #imag + imag
         if type(other) == "imag":
+            #return imag
             return imag(self.val + other.val)
-        
+        #imag + comp
         if type(other) == "comp":
+            #return complex
             return comp(other.real, other.imag + self.val)
-        
+        #imag + real
         else:
+            #return complex
             return comp(other, self.val)
     
     def __sub__(self, other):
